63923961871c51576fec2a94498102988a383b9c
Cyber OS Lab <99838653+cyberoslab@users.noreply.github.com>
2023-03-17 21:18:16 -0700

    Create
    Signed-off-by: Cyber OS Lab. <99838653+cyberoslab@users.noreply.github.com>

---
+++ tools/downloader.go
@@ -0,0 +1,21 @@
+package tools
+
+import (
+	"fmt"
+	"github.com/CVEDB/requests"
+)
+
+func Download(session *requests.Session, rawUrl *string, savePath *string) {
+	resp := session.Get(*rawUrl)
+	if resp == nil {
+		fmt.Printf("[x] 下载失败：%s\n", *rawUrl)
+		return
+	}
+	//tempPath := path.Join(*savePath, path.Base(resp.Request.URL.Path))
+	tmpPath, err := resp.Save(*savePath)
+	if err != nil {
+		fmt.Printf("[x] 下载失败：%s\n", *rawUrl)
+	} else {
+		fmt.Printf("[v] 下载成功：%s，保存路径：%s\n", *rawUrl, tmpPath)
+	}
+}
