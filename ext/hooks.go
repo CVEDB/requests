98e65d69e2f9e25ef2bf9700dd16d4dc486e2fb2
Cyber OS Lab <99838653+cyberoslab@users.noreply.github.com>
2023-03-17 21:27:00 -0700

    Create
    Signed-off-by: Cyber OS Lab. <99838653+cyberoslab@users.noreply.github.com>

---
+++ ext/hooks.go
@@ -0,0 +1,35 @@
+package ext
+
+import "github.com/CVEDB/requests/types"
+
+var defaultHooksList = []string{"request", "client", "response"}
+
+func DefaultHooks() types.HooksDict {
+	hooks := types.HooksDict{}
+	for _, v := range defaultHooksList {
+		hooks[v] = make([]types.Hook, 0)
+	}
+	return hooks
+}
+
+func RegisterHook(hooksDict *types.HooksDict, key string, hook types.Hook) error {
+	hooks := *hooksDict
+	if hooks[key] != nil {
+		hooks[key] = append(hooks[key], hook)
+	} else {
+		hooks[key] = []types.Hook{hook}
+	}
+	return nil
+}
+
+func DisPatchHook(key string, hooks types.HooksDict, data any) any {
+	if hooks[key] != nil {
+		for _, hook := range hooks[key] {
+			err, _hookData := hook(data)
+			if err == nil && _hookData != nil {
+				data = _hookData
+			}
+		}
+	}
+	return data
+}
